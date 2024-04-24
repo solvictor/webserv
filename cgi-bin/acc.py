2953148347c555aceb843a16a62dc84ba9bab8e6
Victor Egret <victor.egret.pro@gmail.com>
2024-04-24 20:16:37 +0200

    Better acc

--- cgi-bin/acc.py
+++ cgi-bin/acc.py
@@ -7,26 +7,21 @@ import hashlib
 import pickle
 import sys
 from http import cookies
-from logger import log
+from logger import *
 from collections import defaultdict
 
-RESET = "\033[0m"
-BOLD = "\033"
-RED = "\033[31m"
-LIGHT_RED = "\033[1;31m"
-GREEN = "\033[32m"
-LIGHT_GREEN = "\033[1;32m"
-YELLOW = "\033[33m"
-LIGHT_YELLOW = "\033[1;33m"
-BLUE = "\033[34m"
-LIGHT_BLUE = "\033[1;34m"
+SESSION_PATH = "cgi-bin/sessions"
 
+if not os.path.exists(SESSION_PATH):
+	os.umask(0)
+	os.mkdir(SESSION_PATH)
 
 class Session:
 	def __init__(self, name):
 		self.name = name
 		self.sid = hashlib.sha1(str(time.time()).encode("utf-8")).hexdigest()
-		with open("cgi-bin/sessions/session_" + self.sid, "wb") as f:
+
+		with open(SESSION_PATH + "/session_" + self.sid, "wb") as f:
 			pickle.dump(self, f)
 
 
@@ -148,7 +143,7 @@ if "HTTP_COOKIE" in os.environ:
 
 	if "SID" in env_cookies:
 		log("Your Session ID is", env_cookies["SID"].value, color=BLUE, file=sys.stderr)
-		with open("cgi-bin/sessions/session_" + env_cookies["SID"].value, "rb") as f:
+		with open(SESSION_PATH + "/session_" + env_cookies["SID"].value, "rb") as f:
 			sess = pickle.load(f)
 			printAccPage(sess)
 	else:
